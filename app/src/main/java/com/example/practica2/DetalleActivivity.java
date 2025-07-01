9d9be9acfdb180fde97c0b1e30179fd22e5e5472
kathy solis <yahaida.solis24@gmail.com>
2025-07-01 18:27:47 -0500

    Inicio de Commits Agregar Menu a alguna de las opciones

--- app/src/main/java/com/example/practica2/DetalleActivivity.java
+++ app/src/main/java/com/example/practica2/DetalleActivivity.java
@@ -1,6 +1,8 @@
 package com.example.practica2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -20,10 +22,17 @@ TextView detalleText;
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
         detalleText = findViewById(R.id.textoDetalle);
-
         String mensaje = getIntent().getStringExtra("mensaje");
         detalleText.setText(mensaje);
+
+
+    }
+
+    public void inicio1(View view) {
+        Intent i= new Intent(DetalleActivivity.this, MainActivity2.class);
+        startActivity(i);
     }
 }
